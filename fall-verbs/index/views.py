46335b3e4ca9f7307b1ffbccba3905f5b429cc2d
Svetlana Malinovskaya <soerty97@gmail.com>
2017-03-07 08:48:54 +0300

    Added index views

--- fall-verbs/index/views.py
+++ fall-verbs/index/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+
+def index(request):
+    """Представление для главной страницы"""
+    return render(request, 'index/index.html', {})
