63965250672bd67dc49c67d9ff16329e4ff0eda5
Svetlana Malinovskaya <soerty97@gmail.com>
2017-03-07 09:26:17 +0300

    Register all models in 'words/admin.py'

--- fall-verbs/words/admin.py
+++ fall-verbs/words/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Word
+from .models import Sense
+from .models import Example
+from .models import Task
+
+
+
+
+# Регистрируем все модели базы данных в панели администратора
+admin.site.register(Word)
+admin.site.register(Sense)
+admin.site.register(Example)
+admin.site.register(Task)
